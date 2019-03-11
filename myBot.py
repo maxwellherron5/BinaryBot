0a79823853f91cc18ff3f9cef079a91987dfcba0
Maxwell <maxwell@Maxwells-MacBook-Pro.local>
2019-03-11 02:19:57 -0500

    eh I tried

--- myBot.py
+++ myBot.py
@@ -28,8 +28,64 @@ def run_bot(r, comments_replied_to):
     print("Scanning for trigger statement...")
 
     for comment in r.subreddit('totallynotrobots').comments(limit=10):
-        if "Binary me!" in comment.body and comment.id not in comments_replied_to and comment.author != r.user.me():
+        if "Binary me!" in comment.body:
+            print("String with trigger word found in comments!")
+
             comment_reply = "Hello fellow human! You have triggered the BinaryBot! Your phrase in binary is: "
 
+            comment.reply(comment_reply + identifyFormat(comment))
+            print("Replied to comment: " + comment.id)
+
+            comments_replied_to.append(comment.id)
+
+            with open("comments_replied_to.txt", "a") as f:
+                f.write(comment.id + "\n")
+
+        print("Sleeping for 10 seconds...")
+        # Sleep for 10 seconds...
+        time.sleep(10)
+
+def identifyFormat(comment):
+    output = []
+
+    if isinstance(comment, int):
+        output += " " + decToBinary(comment)
+    return str(output)
 
 
+def decToBinary(input):
+    # array to store
+    # binary number
+    binaryNum = [0] * input;
+
+    # counter for binary array
+    i = 0;
+    while (input > 0):
+        # storing remainder
+        # in binary array
+        binaryNum[i] = input % 2;
+        input = int(input / 2);
+        i += 1;
+
+    return str(binaryNum)
+
+def get_saved_comments():
+    if not os.path.isfile("comments_replied_to.txt"):
+        comments_replied_to = []
+    else:
+        with open("comments_replied_to.txt", "r") as f:
+            comments_replied_to = f.read()
+            comments_replied_to = comments_replied_to.split("\n")
+            comments_replied_to = list(filter(None, comments_replied_to))
+
+    return comments_replied_to
+
+
+#def toAscii(input):
+
+
+r = bot_login()
+comments_replied_to = get_saved_comments()
+
+while True:
+    run_bot(r, comments_replied_to)
